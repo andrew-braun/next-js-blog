046161271856fab61097cb5995ba3cda1fafd8de
Andrew Braun <andrew@andrewbraun.dev>
2021-04-26 08:30:35 +0300

    style: Refine logo

--- components/Layout/logo.js
+++ components/Layout/logo.js
@@ -1,7 +1,7 @@
 import styles from "./logo.module.css"
 
 function Logo() {
-	return <div className={styles.logo}>Next</div>
+	return <div className={styles.logo}>Next Blog</div>
 }
 
 export default Logo
