7d734949a1ce4b908acaf465721e4267596cae2a
Andrew Braun <andrew@andrewbraun.dev>
2021-05-05 22:04:58 +0400

    feat: Add image processing to React-Markdown

--- components/posts/post-detail/post-content/index.js
+++ components/posts/post-detail/post-content/index.js
@@ -1,4 +1,5 @@
 import ReactMarkdown from "react-markdown"
+import Image from "next/image"
 import PostHeader from "../post-header/index"
 import styles from "./post-content.module.css"
 
@@ -9,11 +10,34 @@ function PostContent(props) {
 	const { title, image, slug } = post.data
 	const content = post.content
 
+	const customRenderers = {
+		p(paragraph) {
+			const { node } = paragraph
+
+			if (node.children[0].tagName === "img") {
+				const image = node.children[0]
+
+				return (
+					<div className={styles.image}>
+						<Image
+							src={`/images/posts/${slug}/${image.properties.src}`}
+							alt={image.alt}
+							width={600}
+							height={300}
+							layout="responsive"
+						/>
+					</div>
+				)
+			}
+			return <p>{paragraph.children}</p>
+		},
+	}
+
 	return (
 		<article className={styles.postContent}>
 			<PostHeader title={title} image={`/images/posts/${slug}/${image}`} />
 			<div className={styles.postText}>
-				<ReactMarkdown>{content}</ReactMarkdown>
+				<ReactMarkdown components={customRenderers}>{content}</ReactMarkdown>
 			</div>
 		</article>
 	)
